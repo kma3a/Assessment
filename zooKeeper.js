45afd6bf0ad3fb687750882d1b8d69503e0aacb1
Kelsey Ando <kma3a@virginia.edu>
2015-03-09 11:27:34 -0400

    adding function to start the game

--- zooKeeper.js
+++ zooKeeper.js
@@ -10,4 +10,13 @@ function Player(name) {
 	this.name = name;
 }
 
-	
+var zooKeeper;
+var myTiger;
+
+function startGame() {
+	console.log("Welcome to the Detroit Labs Zoo");
+	console.log("We are proud you to have you join us as a new zoo keeper");
+	var zooKeeperName = sget("What is your name again?");
+	zooKeeper = new Player(zooKeeperName);
+	console.log("Welcome " + zooKeeper.name + " You will be in charge of our new baby tiger.");
+}		
