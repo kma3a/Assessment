e4f95fab008d8d510252f1ec07d5680553905f8c
Kelsey Ando <kma3a@virginia.edu>
2015-03-09 10:19:01 -0400

    adding the function to get the temperature from the user

--- weeklyTemperature.js
+++ weeklyTemperature.js
@@ -2,3 +2,9 @@ var readline = require('readline');
 var prompt = readline.createInterface(process.stdin, process.stdout);
 
 var weeklyTemperatures = new Array(7);
+
+function getTemperature() {
+	prmopt.question("Enter the temperature: ", function(inputTemperature){
+		checkUserInput(inputTemperature);
+	})
+}
