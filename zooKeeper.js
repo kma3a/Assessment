50ddd84484704a1c78a24ae97249b88dd02c1254
Kelsey Ando <kma3a@virginia.edu>
2015-03-09 11:28:49 -0400

    adding sget

--- zooKeeper.js
+++ zooKeeper.js
@@ -1,3 +1,5 @@
+var sget = require('sget');
+
 function Tiger(name) {
 	this.name = name;
 	this.hungry = 3;
@@ -16,7 +18,7 @@ var myTiger;
 function startGame() {
 	console.log("Welcome to the Detroit Labs Zoo");
 	console.log("We are proud you to have you join us as a new zoo keeper");
-	var zooKeeperName = sget("What is your name again?");
+	var zooKeeperName = sget("What is your name again?").trim();
 	zooKeeper = new Player(zooKeeperName);
 	console.log("Welcome " + zooKeeper.name + " You will be in charge of our new baby tiger.");
 }		
