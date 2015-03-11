e8751baa3b027ee9e269e2411654a2b4ff322228
Kelsey Ando <kma3a@virginia.edu>
2015-03-11 14:22:51 -0400

    adding functions as prototype and adding the hasMeds to person

--- zooKeeper.js
+++ zooKeeper.js
@@ -6,47 +6,71 @@ function Tiger(name) {
 	this.energy = 6;
 	this.healthy = true;
 	this.tired = 5;
-	this.sleep = function(){
-		this.hungry +=2;
-		this.tired -=3;
-		this.energy += 1;
+	this.dead = false;
+
+}
+
+	Tiger.prototype.sleep = function(){
+		if (this.tired > 0) {	
+			this.hungry +=2;
+			this.tired -=3;
+			this.energy += 1;
+			console.log(this.name + " slept peacefully.");
+		} else {
+			console.log( this.name + " is not tired and bites you!");
+		}	
 	};
-	this.learn = function() {
-		this.energy -=3;
-		this.tired += 2;
-		this.hungry +=1;
+
+	Tiger.prototype.learn = function() {
+		if (this.energy > 0) {
+			this.energy -=3;
+			this.tired += 2;
+			this.hungry +=1;
+			console.log(this.name + " learned how to growl on command!");
+		} else {
+			console.log(this.name + " is too tired to move and bites you!");
+		}
 	};
-	this.feed = function() {
-		this.hungry -=3;
-		this.energy +=2;
-		this.tired +=1;
+
+	Tiger.prototype.feed = function() {
+		if (this.hungry > 0 )	{
+			this.hungry -=3;
+			this.energy +=2;
+			this.tired +=1;
+			console.log(this.name + " ate happyly!");
+		} else {
+			console.log(this.name + " is not hungry and bites you!");
+		}
 	};
-	this.checkHealthStatus = function() {
+
+	Tiger.prototype.checkHealthStatus = function() {
 		console.log(this.name + " the Tiger is: ");
 		console.log("Tired: " + this.tired );
 		console.log("Energy: " + this.energy );
 		console.log("Hungry: " + this.hungry );
 		console.log("Healthy: " + this.healthy );
 	};
-	this.checkHealth = function() {
+
+	Tiger.prototype.checkHealth = function() {
 		if (this.energy <2 || this.tired > 9 || this.hungry > 9) {
 			this.healthy = false;
 		}
 	};
-	this.giveMeds = function() {
-	   if (this.healthy === false) {
-		this.tired = 7;
-		this.hungry = 5;
-		this.energy = 4;
-		this.healthy = true;
-	  } else{
-		console.log("You can't give a healthy tiger medicine are you crazy?!?!?!?");
-	}
+
+	Tiger.prototype.giveMeds = function(haveMeds) {
+		if (this.healthy === false && haveMeds <= 0) {
+			this.tired = 7;
+			this.hungry = 5;
+			this.energy = 4;
+			this.healthy = true;
+	  	} else{
+			console.log("You can't give a healthy tiger medicine are you crazy?!?!?!?");
+		}
 	};
-}
 
 function Player(name) {
 	this.name = name;
+	this.hasMeds = 2;
 }
 
 var zooKeeper;
@@ -54,54 +78,77 @@ var myTiger;
 
 function startGame() {
 	console.log(" Welcome to the Detroit Labs Zoo\n We are proud you to have you join us as a new zoo keeper");
-	var zooKeeperName = sget("What is your name again?").trim();
-	zooKeeper = new Player(zooKeeperName);
-	console.log(" Welcome " + zooKeeper.name + " You will be in charge of our new baby tiger.");
-	var tigerName = sget("What would you like to name the tiger?").trim();
-	myTiger = new Tiger(tigerName);
+	getKeeperName();
+	getTigerName();
 	console.log(" Great ZooKeeper " + zooKeeper.name + ", you will be in charge of " + myTiger.name + " the Tiger.\n be careful " + myTiger.name + " might bite!.");
 	chooseTask();
 }	
 
+function getKeeperName() {
+	var zooKeeperName = getWord("What is your name again?");
+	zooKeeper = new Player(zooKeeperName);
+	console.log(" Welcome " + zooKeeper.name + " You will be in charge of our new baby tiger.");
+}
+
+function getTigerName(){
+	var tigerName = getWord("What would you like to name the tiger?");
+	myTiger = new Tiger(tigerName);
+}
+
+function getWord(saying) {
+	var word = sget(saying).trim();
+	return checkValid(word);
+}
+
+function checkValid(word) {
+	if (word === '') {
+		return getWord("Sorry that is not valid try again");
+	} else {
+		return word;
+	}
+}
+
+
 function chooseTask() {
-	console.log("feed - Feeds the tiger");
-	console.log("sleep - Lets the tiger sleep");
-	console.log("train - you take time to train the tiger");
-	console.log("check health - you check the health of the tiger");
-	console.log("give meds - gives the tiger meds.");
+	console.log("1 - Feeds the tiger");
+	console.log("2 - Lets the tiger sleep");
+	console.log("3 - you take time to train the tiger");
+	console.log("4 - you check the health of the tiger");
+	console.log("5 - gives the tiger meds.");
 	console.log("exit - quits the game");
 	var userInput = sget("What would you like to do? ").trim();
 	switch (userInput) {
-		case "feed":
-		myTiger.feed();
-		myTiger.checkHealth();
-		chooseTask();
-		break;
-		case "sleep":
-		myTiger.sleep();
-		myTiger.checkHealth();
-		chooseTask();
-		break;
-		case "train":
-		myTiger.learn();
-		myTiger.checkHealth();
-		chooseTask();
-		break;
-		case "check health":
-		myTiger.checkHealthStatus();
-		chooseTask();
-		break;
-		case "give meds":
-		myTiger.giveMeds();
-		chooseTask();
-		break;
+		case '1':
+			myTiger.feed();
+			myTiger.checkHealth();
+	 		chooseTask();
+			break;
+		case '2':
+			myTiger.sleep();
+			myTiger.checkHealth();
+			chooseTask();
+			break;
+		case '3':
+			myTiger.learn();
+			myTiger.checkHealth();
+			chooseTask();
+			break;
+		case '4':
+			myTiger.checkHealthStatus();
+			chooseTask();
+			break;
+		case '5':
+			myTiger.giveMeds();
+			chooseTask();
+			break;
 		case "exit":
-		exitGame();
-		break;
+			exitGame();
+			break;
+		case '':
 		default: 
-		console.log("You can't do that.");
-		chooseTask();
-		break;
+			console.log("You can't do that.");
+			chooseTask();
+			break;
 	}
 }
 
